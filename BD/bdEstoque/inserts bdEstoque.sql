5354a1c49e84d9e8f893196f9d1e380eea0cfd43
Kauanny Tenorio <72138192+KakauFelix@users.noreply.github.com>
2021-05-14 23:41:25 -0300

    Update inserts bdEstoque.sql

--- BD/bdEstoque/inserts bdEstoque.sql
+++ BD/bdEstoque/inserts bdEstoque.sql
@@ -1,67 +1,67 @@
 use bdEstoque
 
 insert into tbCliente (nomeCliente, cpfCliente, emailCliente, sexoCliente, dataNascimentoCliente)
-values ('Armando JosÈ Santana', 12345678900, 'armandojsantana@outlook.com', 'm', '21/02/1961')
+values ('Armando Jos√© Santana', 12345678900, 'armandojsantana@outlook.com', 'm', '21/02/1961')
        , ('Shela Carvalho Leal', 45678909823, 'scarvalho@ig.com.br', 'f', '13/09/1978'  )
-	   , ('Carlos Henrique Souza', 76598278299, 'chenrique@ig.com.br,', 'm', '08/09/1981' )
-	   , ('Maria Aparecida Souza', 87365309899, 'mapdasouza@outlook.com', 'f', '07/07/1962')
-	   , ('Adriana Nogueira Silva', 76354309388, 'drica1977@ig.com.br', 'f', '09/04/1977')
-	   , ('Paulo Henrique Silva', 87390123111, 'phsilva80@hotmail.com', 'm', '02/02/1987')
+       , ('Carlos Henrique Souza', 76598278299, 'chenrique@ig.com.br,', 'm', '08/09/1981' )
+       , ('Maria Aparecida Souza', 87365309899, 'mapdasouza@outlook.com', 'f', '07/07/1962')
+       , ('Adriana Nogueira Silva', 76354309388, 'drica1977@ig.com.br', 'f', '09/04/1977')
+       , ('Paulo Henrique Silva', 87390123111, 'phsilva80@hotmail.com', 'm', '02/02/1987')
 
 select * from tbCliente
 
 insert into tbFabricante (nomeFabricante)
 values ('Unilever')
        , ('P&G')
-	   , ('Bunge')
+       , ('Bunge')
 
 select * from tbFabricante
 
 insert into tbFornecedor (nomeFornecedor, contatoFornecedor)
-values ('Atacad„o', 'Carlos Santos')
+values ('Atacad√£o', 'Carlos Santos')
        , ('Assai', 'Maria Stella')
-	   , ('Rold„o', 'Paulo CÈsar')
+       , ('Rold√£o', 'Paulo C√©sar')
 
 select * from tbFornecedor 
 
-insert into tbProduto (descriÁ„oProduto, valorProduto, quantidadeProduto, codFabricante, codFornecedor)
+insert into tbProduto (descricaoProduto, valorProduto, quantidadeProduto, codFabricante, codFornecedor)
 values ('Amaciante Downy', 5.76, 1500, 2, 1)
        , ('Amaciante Comfort', 5.45, 2300, 1, 1)
-	   , ('Sab„o em pÛ OMO', 5.99, 1280, 1, 2)
-	   , ('Margarina Qually', 4.76, 2500, 3, 1)
-	   , ('Salsicha Hot Dog Sadia', 6.78, 2900, 3, 2)
-	   , ('Mortadela Perdig„o', 2.50, 1200, 3, 3)
-	   , ('Hamburger Sadia', 9.89, 1600, 3, 1)
-	   , ('Fralda Pampers', 36.00, 340, 2, 3)
-	   , ('Xampu Seda', 5.89, 800, 1, 2)
-	   , ('Condicionador Seda', 6.50, 700, 1, 3)
+       , ('Sab√£o em p√≥ OMO', 5.99, 1280, 1, 2)
+       , ('Margarina Qually', 4.76, 2500, 3, 1)
+       , ('Salsicha Hot Dog Sadia', 6.78, 2900, 3, 2)
+       , ('Mortadela Perdig√£o', 2.50, 1200, 3, 3)
+       , ('Hamburger Sadia', 9.89, 1600, 3, 1)
+       , ('Fralda Pampers', 36.00, 340, 2, 3)
+       , ('Xampu Seda', 5.89, 800, 1, 2)
+       , ('Condicionador Seda', 6.50, 700, 1, 3)
 
 select * from tbProduto
 
 insert into tbVenda (dataVenda, valorTotalVenda, codCliente)
 values ('01/02/2014', 4500, 1)
        , ('03/02/2014', 3400, 1)
-	   , ('10/02/2014', 2100, 2)
-	   , ('15/02/2014', 2700, 3)
-	   , ('17/03/2014', 560, 3)
-	   , ('09/04/2014', 1200, 4)
-	   , ('07/05/2014', 3500, 5)
-	   , ('07/05/2014', 3400, 1)
-	   , ('05/05/2014', 4000, 2)
+       , ('10/02/2014', 2100, 2)
+       , ('15/02/2014', 2700, 3)
+       , ('17/03/2014', 560, 3)
+       , ('09/04/2014', 1200, 4)
+       , ('07/05/2014', 3500, 5)
+       , ('07/05/2014', 3400, 1)
+       , ('05/05/2014', 4000, 2)
 
 select * from tbVenda 
 
 insert into tbItensVenda (codVenda, codProduto, quantidadeItensVenda, subTotalItensVenda)
 values (1, 1, 200, 1500)
        , (1, 2, 300, 3000)
-	   , (2, 4, 120, 1400)
-	   , (2, 2, 200, 1000)
-	   , (2, 3, 130, 1000)
-	   , (3, 5, 200, 2100)
-	   , (4, 4, 120, 1000)
-	   , (4, 5, 450, 700)
-	   , (5, 5, 200, 560)
-	   , (6, 7, 200, 600)
-	   , (6, 6, 300, 600)
+       , (2, 4, 120, 1400)
+       , (2, 2, 200, 1000)
+       , (2, 3, 130, 1000)
+       , (3, 5, 200, 2100)
+       , (4, 4, 120, 1000)
+       , (4, 5, 450, 700)
+       , (5, 5, 200, 560)
+       , (6, 7, 200, 600)
+       , (6, 6, 300, 600)
 
 select * from tbItensVenda
